6c5646a212d6f22c85e793f83ce812ff8c8a13c5
Amber LIU <AmberLiu@Ambers-MacBook.local>
2017-12-11 14:56:51 -0800

    basic framework

--- travel/src/App.js
+++ travel/src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Landing from './Landing';
+import Detail from './Detail';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+            <Route exact path='/' component={Landing}/>
+            <Route exact path='/detail/' component={Detail}/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ travel/src/Landing/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  render() {
+    return (
+      <div> Landing </div>
+    );
+  }
+}
+
+export default Landing;
